4f10f1741b85baa8b1e997697ad396abaf30128a
diegusxavier <dxavier.mac@gmail.com>
2024-03-04 16:24:29 -0300

    no found address mapping.py bug fix

--- src/mapping/mapping.py
+++ src/mapping/mapping.py
@@ -6,26 +6,27 @@ gmaps = googlemaps.Client(key='AIzaSyCzQIxBQDxD-9apyT4gdRzcKVCgsGi1cBM')
 
 def get_coordinates(addres):
     geocode_result = gmaps.geocode(addres)
-    # if geocode_result:
-    lat = geocode_result[0]['geometry']['location']['lat']
-    lon = geocode_result[0]['geometry']['location']['lng']
-    print(lat, lon)
-    # else:
-    #     print('No results found for the given address:', addres)
-    return lat, lon
+    if geocode_result:
+        lat = geocode_result[0]['geometry']['location']['lat']
+        lon = geocode_result[0]['geometry']['location']['lng']
+        coordinates = [lat, lon]
+        # print(coordinates)
+        return lat, lon
+    else:
+        # print('Não encontrado:', addres)
+        return []
+    
     
 
 def plot_map(dataframe, nome):
-    lat, lon = get_coordinates(dataframe['Localização'][0])
-    map = folium.Map(location=[lat, lon], zoom_start=12)
+    coordinates = get_coordinates(dataframe['Localização'][0])
+    map = folium.Map(location=coordinates, zoom_start=12)
 
     for i in range(dataframe.shape[0]):
-        lat, lon = get_coordinates(dataframe['Localização'][i])
-        print(i, dataframe['Localização'][i])
-        folium.CircleMarker(location=[lat, lon], fill=True, radius=8, tooltip=(f'Valor do leilão: R$ {dataframe.iloc[i, 0]}')).add_to(map)
-        if i == 80:
-            break
+        coordinates = get_coordinates(dataframe['Localização'][i])
+        if len(coordinates) == 0:
+            print('Endereço não encontrado:', dataframe['Link'][i])
+            continue
+        folium.CircleMarker(location=coordinates, fill=True, radius=8, tooltip=(f'Valor do leilão: R$ {dataframe.iloc[i, 0]:,}'), popup=dataframe['Link'][i]).add_to(map)
     map.save(f'output//map_{nome}.html')
 
-dataframe = pd.read_excel('output\leilaoimoveis.xlsx')
-plot_map(dataframe, 'fortaleza-ce')
