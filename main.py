cfd7f2bb6378bf203c95a974b21e2615ac568e04
diegusxavier <dxavier.mac@gmail.com>
2024-03-05 15:51:47 -0300

    add main functions

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ from src.mapping.mapping import *
 
 def main():
     # inicia o prompt perguntando o que fazer
-    print('Qual operação você deseja realizar?\n  1 - pegar dados do site leilaoimoveis.com.br\n  2 - fazer mapeamento das areas')
+    print('----\nQual operação você deseja realizar?\n  1 - Extrair dados de um site\n  2 - Fazer mapeamento dos imóveis da região')
     input_command = int(input('Escolha o número da operação desejada: '))
 
     if input_command == 1:
-        website = r'https://www.leilaoimovel.com.br/leilao-de-imovel/eusebio-ce'
-        create_n_save_df(website)
+        chose_extract_data()
     elif input_command == 2:
-        dataframe = pd.read_excel('output\leilaoimoveis.xlsx')
+        chose_plot_map()
+
+def chose_extract_data():
+    print('----\nQual site você deseja acessar?\n  1 - leilaoimoveis.com.br\n  2 - ')
+    input_command = int(input('Escolha o número da operação desejada: '))
+    if input_command == 1:
+        print('----\nQual o município desejado?\n  1 - Fortaleza (CE)\n  2 - Eusébio (CE)')
+        input_command = int(input('Escolha o número referente ao estado desejado: '))
+        if input_command == 1:
+            create_n_save_df(r'https://www.leilaoimovel.com.br/leilao-de-imovel/fortaleza-ce')
+        elif input_command == 2:
+            create_n_save_df(r'https://www.leilaoimovel.com.br/leilao-de-imovel/eusebio-ce')
+
+def chose_plot_map():
+    print('----\nQual mapa você deseja plotar\n  1 - Fortaleza (CE)\n  2 - Eusébio (CE)')
+    input_command = int(input('Escolha o número referente ao mapa escolhido: '))
+    if input_command == 1:
+        dataframe = pd.read_excel('output\leilaoimoveis_fortaleza-ce.xlsx')
         plot_map(dataframe, 'fortaleza-ce')
-
-
+    elif input_command == 2:
+        dataframe = pd.read_excel('output\leilaoimoveis_eusebio-ce.xlsx')
+        plot_map(dataframe, 'eusebio-ce')
 
 if __name__ == '__main__':
     main()
