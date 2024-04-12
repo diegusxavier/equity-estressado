10f73b57b02bbc595e836f2a88db8e8a7d7ec159
diegusxavier <dxavier.mac@gmail.com>
2024-04-12 11:02:05 -0300

    create get_page_json function

--- src/scraping/scraping_olx.py
+++ src/scraping/scraping_olx.py
@@ -2,6 +2,7 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -14,14 +15,33 @@ def setup_webdriver():
     driver = webdriver.Firefox(service=service, options=options)
     return driver
 
-def extract_data(link, driver):
-    driver.get(link)
+# verify if page exists
+def page_exists(website, driver):
+    driver.get(website)
+    if 'Nenhum anúncio' in driver.find_element(By.XPATH, r'/html/body/div[1]/div[1]/main/div/div[2]/main/div[4]').text:
+        print('Page not found')
+        return False
+    return True
 
-    # details
-    area = None
-    bathdrooms = None
-    bedrooms = None
-    car_vacancies = None
+def get_page_json(website, driver, list_ads):
+    for i in range(1,101):
+        new_website = website + f'?o={i}'
+        if page_exists(new_website, driver):
+            element = driver.find_element(By.ID, '__NEXT_DATA__')
+            json_text = driver.execute_script('return arguments[0].innerHTML;', element)
+            data = json.loads(json_text)
+            page_ads = data['props']['pageProps']['ads']
+            for page_ad in page_ads:
+                list_ads.append(page_ad)
+            print(len(list_ads))
+    save_list(list_ads)
 
-    condominium_features = []
-    property_features = []
+def save_list(list):
+    with open("file.txt", 'w', encoding='utf-8') as f:
+        for item in list:
+            f.write(str(item) + '\n')
+
+
+driver = setup_webdriver()
+list_ads = []
+get_page_json('https://www.olx.com.br/imoveis/venda/estado-ce/fortaleza-e-regiao', driver, list_ads)
